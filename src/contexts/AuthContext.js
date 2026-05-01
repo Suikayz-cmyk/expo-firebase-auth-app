ae183fc136d46199e9167c7c343420113babb0d1
Muhammad Prayogo Pangestu <prayogoxyz@gmail.com>
2026-05-01 23:17:14 +0700

    feat: implement 5-minute idle auto logout

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,4 +1,5 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState, useRef } from 'react';
+import { AppState, Alert } from 'react-native';
 import { onAuthStateChanged, signOut } from 'firebase/auth';
 import * as SecureStore from 'expo-secure-store';
 import { auth } from '../config/firebase';
@@ -8,9 +9,10 @@ const AuthContext = createContext();
 export const useAuth = () => useContext(AuthContext);
 
 export function AuthProvider({ children }) {
-
  const [user, setUser] = useState(null);
  const [loading, setLoading] = useState(true);
+
+ const backgroundTimeRef = useRef(null);
 
  useEffect(() => {
     const unsub = onAuthStateChanged(auth, async (u) => {
@@ -19,7 +21,7 @@ export function AuthProvider({ children }) {
             const token = await u.getIdToken();
             await SecureStore.setItemAsync('auth_token', token);
         } else {
-            //await SecureStore.deleteItemAsync('auth_token');
+            await SecureStore.deleteItemAsync('auth_token');
         }
         setLoading(false);
     });
@@ -28,9 +30,39 @@ export function AuthProvider({ children }) {
     }, []);
 
  const logout = async () => {
-    await signOut(auth);
-   // await SecureStore.deleteItemAsync('auth_token');
+   await signOut(auth);
+   await SecureStore.deleteItemAsync('auth_token');
  };
+
+ useEffect(() => {
+  const subscription = AppState.addEventListener(
+    'change',
+    async (nextState) => {
+      if (nextState === 'background') {
+        backgroundTimeRef.current = Date.now();
+      }
+
+      if (nextState === 'active') {
+        if (backgroundTimeRef.current && user) {
+          const diff =
+            Date.now() - backgroundTimeRef.current;
+
+          if (diff >= 10000) { 
+            await logout();
+
+            Alert.alert(
+              'Logout Otomatis',
+              'Anda logout karena idle 5 menit.'
+            );
+          }
+        }
+      }
+    }
+  );
+
+  return () => subscription.remove();
+}, [user]);
+
 
  return (
     <AuthContext.Provider value={{ user, loading, logout }}>
